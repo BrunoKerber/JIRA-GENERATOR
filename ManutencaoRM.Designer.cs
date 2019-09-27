030a790fc4ffeab79c6d3e49bcfd95564f6ccfcb
BrunoKerber <bruno.kerber95@gmail.com>
2019-09-27 14:37:34 -0300

    Arquivos iniciais do projeto

---
+++ ManutencaoRM.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Jira_Generator_V2
+{
+    partial class ManutencaoRM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManutencaoRM));
+            this.SuspendLayout();
+            // 
+            // ManutencaoRM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(764, 541);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ManutencaoRM";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Relato de Problema (RM)";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
